550b4f1843ff056dc6769fb0b7e5cf65a323599e
Henry Summers <ssumo89@gmail.com>
2016-06-29 23:47:10 +0100

    first commit - noun vars

---
+++ homework-week-01/madlibs.js
@@ -0,0 +1,9 @@
+//adjective, noun, verb, place, people
+
+var adjective = ['active', 'big', 'complex', 'dim', 'elegant', 'filthy'];
+var noun = ['oven', 'country', 'week', 'exam', 'movie', 'series', 'camera'];
+var person = ['John', 'Fred', 'Cindy', 'Donald', 'Mark'];
+var place = ['France', 'Turkey', 'Waitrose', 'Chariots', 'Browns', 'Islington', 'Scunthorpe'];
+var verb = ['admire', 'boil', 'contain', 'dial', 'emit', 'floss', 'help'];
+
+var story = 'Once upon a time there was a [adjective] [noun]. It was really [adjective]. It liked to [verb] all day. One day, it went to [place] to meet [person]. To get there, it rode in a [vehicle], but on the way there, the [vehicle] crashed.  It had to walk the rest of the way.'
