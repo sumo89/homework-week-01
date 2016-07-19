59c891046228930b47328eecc78467bcc34997c3
Henry Summers <ssumo89@gmail.com>
2016-07-19 19:47:57 +0100

    trying to fix

--- homework-week-01/madlibs.js
+++ homework-week-01/madlibs.js
@@ -7,12 +7,12 @@ var verb = ['admire', 'boil', 'contain', 'dial', 'emit', 'floss', 'help'];
 var vehicle = ['car', 'bike', 'tricycle', 'train', 'pogo stick'];
 
 //randoms
-var randomAdjective = adjective[Math.floor(Math.random() * adjective.length)];
-var randomNoun = noun[Math.floor(Math.random() * noun.length)];
-var randomPerson = person[Math.floor(Math.random() * person.length)];
-var randomPlace = place[Math.floor(Math.random() * place.length)];
-var randomVerb = verb[Math.floor(Math.random() * verb.length)];
-var randomVehicle = vehicle[Math.floor(Math.random() * vehicle.length)];
+var randomAdjective 	= adjective[Math.floor(Math.random() 	* adjective.length)];
+var randomNoun 			= noun[Math.floor(Math.random() 		* noun.length)];
+var randomPerson 		= person[Math.floor(Math.random() 		* person.length)];
+var randomPlace 		= place[Math.floor(Math.random() 		* place.length)];
+var randomVerb 			= verb[Math.floor(Math.random() 		* verb.length)];
+var randomVehicle 		= vehicle[Math.floor(Math.random() 		* vehicle.length)];
 
 //story
 var story = 'Once upon a time there was a ' + randomAdjective + ' ' + randomNoun + '. It was really ' + randomAdjective + '. It liked to ' + randomVerb + ' all day. One day, it went to ' + randomPlace + ' to meet ' + randomPerson + '. To get there, it rode in a ' + randomVehicle + ', but on the way there, the ' + randomVehicle + ' crashed. It had to walk the rest of the way.';
